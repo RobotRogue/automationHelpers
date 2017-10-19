7aa4df455fbadf5f66edc24488a0508407474c68
Nelson Gill <RobotRogue@users.noreply.github.com>
2017-10-19 09:27:11 -0700

    Added uppercase/lowercase/number change
    Changed format to output Uppercase + lowercase + number for more randomization.

--- generateTextString.js
+++ generateTextString.js
@@ -1,18 +1,21 @@
 // Generate Random Text String
 
 function makeid() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var rest = "";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var numeric = "0123456789";
 
-  for (var i = 0; i < 5; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-  return text;
+  first = uppercase.charAt(Math.floor(Math.random() * 10))
+  numval = numeric.charAt(Math.floor(Math.random() * 10))
+  
+  for (var i = 0; i < 4; i++)
+    rest += lowercase.charAt(Math.floor(Math.random() * lowercase.length));
+    return first + rest + numval;
 }
 
 console.log(makeid());
 
 /*
-TODO: Modify to make only the first char uppercase.
 NOTE: When using this in test, make sure the test outputs/logs the value generated, in the event you need it to troubleshoot.
 */
